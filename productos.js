b26fab2cf79a28ab6bd1824e1ac37600d167a6ee
smlopez30 <smlopez@gmail.com>
2023-04-27 20:24:00 -0300

    Update productos.js

--- productos.js
+++ productos.js
@@ -16,15 +16,39 @@ for (var i = 0; i < popups.length; i++) {
   });
 }
 
-// Agregar evento de click adicional a la imagen
+// Agregar evento de click o touch a la imagen
 document.querySelectorAll(".product-img").forEach(img => {
   img.addEventListener("click", function() {
-    // Verificar si el detalle está visible
-    if (this.nextSibling.style.display === "block") {
-      hidePopup(this);
-    } else {
-      showPopup(this);
+    togglePopup(this);
+  });
+  
+  img.addEventListener("touchstart", function(event) {
+    // evita el doble click en dispositivos móviles
+    if (event.touches.length === 1) {
+      togglePopup(this);
     }
   });
 });
+
+// funcion para mostrar u ocultar el detalle
+function togglePopup(img) {
+  // Verificar si el detalle está visible
+  if (img.nextSibling.style.display === "block") {
+    hidePopup(img);
+  } else {
+    showPopup(img);
+  }
+}
+
+// Agregar evento de click a otras partes de la pantalla para ocultar el detalle
+document.addEventListener("click", function(event) {
+  if (!event.target.classList.contains("product-img") && 
+      !event.target.classList.contains("popup")) {
+    popups.forEach(popup => {
+      popup.style.display = "none";
+    });
+  }
+});
+
+
   
