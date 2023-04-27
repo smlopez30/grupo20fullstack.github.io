f9e7bda44d0dceea7f674a89125cb4e628f8ea79
smlopez30 <smlopez@gmail.com>
2023-04-27 20:49:31 -0300

    Update productos.js

--- productos.js
+++ productos.js
@@ -1,51 +1,55 @@
+// Mostrar el detalle
 function showPopup(img) {
   var popup = img.nextElementSibling;
   popup.style.display = "block";
 }
 
+// Ocultar el detalle
 function hidePopup(img) {
   var popup = img.nextElementSibling;
   popup.style.display = "none";
 }
 
-
-var popups = document.querySelectorAll(".popup");
-for (var i = 0; i < popups.length; i++) {
-  popups[i].addEventListener("click", function () {
-      this.style.display = "none";
-  });
-}
-
 // Agregar evento de click o touch a la imagen
 document.querySelectorAll(".product-img").forEach(img => {
+  // variable para verificar si el detalle está abierto
+  var isOpen = false;
+  
   img.addEventListener("click", function(event) {
     event.stopPropagation();
-    togglePopup(this);
+    
+    // Si el detalle está abierto, cerrarlo
+    if (isOpen) {
+      hidePopup(this);
+      isOpen = false;
+    } else {
+      showPopup(this);
+      isOpen = true;
+    }
   });
   
   img.addEventListener("touchstart", function(event) {
     // evita el doble click en dispositivos móviles
     if (event.touches.length === 1) {
-      togglePopup(this);
+      event.stopPropagation();
+      
+      // Si el detalle está abierto, cerrarlo
+      if (isOpen) {
+        hidePopup(this);
+        isOpen = false;
+      } else {
+        showPopup(this);
+        isOpen = true;
+      }
     }
   });
 });
-
-// funcion para mostrar u ocultar el detalle
-function togglePopup(img) {
-  // Verificar si el detalle está visible
-  if (img.nextElementSibling.style.display === "block") {
-    hidePopup(img);
-  } else {
-    showPopup(img);
-  }
-}
 
 // Agregar evento de click a otras partes de la pantalla para ocultar el detalle
 document.addEventListener("click", function(event) {
   if (!event.target.classList.contains("product-img") && 
       !event.target.classList.contains("popup")) {
-    popups.forEach(popup => {
+    document.querySelectorAll(".popup").forEach(popup => {
       popup.style.display = "none";
     });
   }
@@ -55,7 +59,7 @@ document.addEventListener("click", function(event) {
 document.addEventListener("touchend", function(event) {
   if (!event.target.classList.contains("product-img") && 
       !event.target.classList.contains("popup")) {
-    popups.forEach(popup => {
+    document.querySelectorAll(".popup").forEach(popup => {
       popup.style.display = "none";
     });
   }
