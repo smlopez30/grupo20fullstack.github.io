785012d02b80df113c41bd7fcb639fda2b04d7df
smlopez30 <smlopez@gmail.com>
2023-05-29 15:14:04 -0300

    Ultimos retoques

---
+++ script/animacion.js
@@ -0,0 +1,4 @@
+ // Ejecuta la animación al cargar la página
+ window.onload = function() {
+    document.body.classList.add('explosion-animation');
+};
--- script/formulario.js
+++ script/formulario.js
@@ -56,22 +56,3 @@ inputs.forEach((input) => {
 	input.addEventListener('blur', validarFormulario);
 });
 
-formulario.addEventListener('submit', (e) => {
-	e.preventDefault();
-
-	const terminos = document.getElementById('terminos');
-	if(campos.nombre && campos.apellido && campos.correo && campos.telefono && terminos.checked ){
-		formulario.reset();
-
-		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
-		setTimeout(() => {
-			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
-		}, 5000);
-
-		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
-			icono.classList.remove('formulario__grupo-correcto');
-		});
-	} else {
-		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
-	}
-});
