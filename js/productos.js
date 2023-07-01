18b2c2934d8cab03721ba180c0e2257c8e816cae
smlopez30 <smlopez@gmail.com>
2023-07-01 02:15:24 -0300

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -57,7 +57,7 @@ createApp({
             fetch(this.url, options)
                 .then(function () {
                     alert("Registro grabado")
-                    window.location.href = "./productos.html";
+                    window.location.href = "./producto.html";
                 })
                 .catch(err => {
                     console.error(err);
