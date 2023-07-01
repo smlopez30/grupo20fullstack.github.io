860ae8bdf8fe9c04cbe2114add946924b64cdae4
smlopez30 <smlopez@gmail.com>
2023-07-01 01:40:32 -0300

    mas pruebas

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
 navbar-light bg-light">
 <div class="container">
-<a class="navbar-brand" href="../templates/producto.html">CRUD</a>
+<a class="navbar-brand" href="producto.html">CRUD</a>
 
 </div>
 </nav>`
--- js/producto_editar.js
+++ js/producto_editar.js
@@ -47,7 +47,7 @@ redirect: 'follow'
 fetch(this.url, options)
 .then(function () {
 alert("Registro modificado")
-window.location.href = "../templates/productos.html";
+window.location.href = "producto.html";
 })
 .catch(err => {
 console.error(err);
