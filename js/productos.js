07e5b8788436e153d994c6539d7df9c4576777a8
smlopez30 <smlopez@gmail.com>
2023-07-02 16:20:03 -0300

    actializacion js

--- js/productos.js
+++ js/productos.js
@@ -1,72 +1,67 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 createApp({
     data() {
         return {
             productos: [],
-            //url:'http://localhost:5000/productos',
-            // si el backend esta corriendo local usar localhost 5000(si no lo subieron a pythonanywhere)
-            url: 'https://slopez.pythonanywhere.com/productos', // si ya lo subieron a pythonanywhere
+            url: 'https://slopez.pythonanywhere.com/productos',
             error: false,
             cargando: true,
-            /*atributos para el guardar los valores del formulario */
             id: 0,
             nombre: "",
             imagen: "",
             stock: 0,
             precio: 0,
-        }
+        };
     },
     methods: {
         fetchData(url) {
             fetch(url)
-
                 .then(response => response.json())
                 .then(data => {
-                    this.producto = data;
-                    this.cargando = false
+                    this.productos = data;
+                    this.cargando = false;
                 })
                 .catch(err => {
                     console.error(err);
-                    this.error = true
-                })
+                    this.error = true;
+                });
         },
         eliminar(producto) {
             const url = this.url + '/' + producto;
             var options = {
                 method: 'DELETE',
-            }
+            };
             fetch(url, options)
-                .then(res => res.text()) // or res.json()
+                .then(res => res.text())
                 .then(res => {
                     location.reload();
-                })
+                });
         },
         grabar() {
             let producto = {
                 nombre: this.nombre,
                 precio: this.precio,
                 stock: this.stock,
-                imagen: this.imagen
-            }
+                imagen: this.imagen,
+            };
             var options = {
                 body: JSON.stringify(producto),
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                redirect: 'follow'
-            }
+                redirect: 'follow',
+            };
             fetch(this.url, options)
-                .then(function () {
-                    alert("Registro grabado")
-                    window.location.href = "./producto.html";
+                .then(() => {
+                    alert("Registro grabado");
+                    this.fetchData(this.url);
                 })
                 .catch(err => {
                     console.error(err);
-                    alert("Error al Grabarr")
-
-                })
-        }
+                    alert("Error al grabar");
+                });
+        },
     },
     created() {
-        this.fetchData(this.url)
+        this.fetchData(this.url);
     },
-}).mount('#app')
+}).mount('#app');
