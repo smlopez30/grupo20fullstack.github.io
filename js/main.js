c4d7a280bdb0990966c8f493a3ca3d8704a64d12
smlopez30 <smlopez@gmail.com>
2023-07-02 20:16:30 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
-navbar-light bg-light">
-<div class="container">
-/* <a class="navbar-brand" href="producto.html">CRUD</a> */
-
-</div>
-</nav>`
+document.getElementById("header").innerHTML = `
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="producto.html">CRUD</a>
+        </div>
+    </nav>
+    <section class="textos-header">
+        <img src="../imagenes/Logo.jpeg" width="50" height="50" alt="Logo">
+        <h1>Tu Tabaqueria</h1>
+        <h2>Encuentranos </h2>
+    </section>
+    <div class="wave" style="height: 150px; overflow: hidden;">
+        <svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
+            <path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
+                style="stroke: none; fill: #ffffff;"></path>
+        </svg>
+    </div>
+`;
