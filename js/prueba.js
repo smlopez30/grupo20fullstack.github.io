e4487991821d55bb01213b7ee5c1b2f8269ff5d2
smlopez30 <smlopez@gmail.com>
2023-07-03 17:46:33 -0300

    Create prueba.js

---
+++ js/prueba.js
@@ -0,0 +1,108 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            productos: [],
+            url: 'https://slopez.pythonanywhere.com/productos',
+            error: false,
+            cargando: true,
+            pageSize: 5,
+            currentPage: 1,
+            nuevoProducto: {
+                nombre: '',
+                precio: '',
+                stock: '',
+                imagen: ''
+            },
+        };
+    },
+    computed: {
+        totalItems() {
+            return this.productos.length;
+        },
+        totalPages() {
+            return Math.ceil(this.totalItems / this.pageSize);
+        },
+        paginatedProductos() {
+            const startIndex = (this.currentPage - 1) * this.pageSize;
+            const endIndex = startIndex + this.pageSize;
+            return this.productos.slice(startIndex, endIndex);
+        },
+    },
+    methods: {
+        fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    this.productos = data;
+                    this.cargando = false;
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error = true;
+                });
+        },
+        eliminar(id) {
+            const url = this.url + '/' + id;
+            var options = {
+                method: 'DELETE',
+            };
+            fetch(url, options)
+                .then(res => res.text())
+                .then(res => {
+                    this.fetchData(this.url);
+                });
+        },
+        actualizar(producto) {
+            const url = this.url + '/' + producto.id;
+            var options = {
+                body: JSON.stringify(producto),
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                redirect: 'follow',
+            };
+            fetch(url, options)
+                .then(() => {
+                    alert("El artículo se ha actualizado correctamente.");
+                })
+                .catch(err => {
+                    console.error(err);
+                    alert("Error al actualizar el artículo.");
+                });
+        },
+        agregarNuevo() {
+            if (this.nuevoProducto.nombre && this.nuevoProducto.precio && this.nuevoProducto.stock) {
+                var options = {
+                    body: JSON.stringify(this.nuevoProducto),
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    redirect: 'follow',
+                };
+                fetch(this.url, options)
+                    .then(() => {
+                        alert("El artículo se ha guardado correctamente.");
+                        this.fetchData(this.url);
+                        this.nuevoProducto = {
+                            nombre: '',
+                            precio: '',
+                            stock: '',
+                            imagen: ''
+                        };
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        alert("Error al guardar el artículo.");
+                    });
+            } else {
+                alert("Por favor, complete todos los campos obligatorios.");
+            }
+        },
+        changePage(page) {
+            this.currentPage = page;
+        },
+    },
+    created() {
+        this.fetchData(this.url);
+    },
+}).mount('#app');
