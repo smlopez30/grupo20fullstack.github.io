6ad3d89a264362a9f820862ced91a5369f949690
smlopez30 <smlopez@gmail.com>
2023-07-05 12:40:08 -0300

    Update mostrar.js

--- js/mostrar.js
+++ js/mostrar.js
@@ -22,7 +22,7 @@ function mostrarProductos(productos) {
     const productosFiltrados = productos.filter(producto => producto.mostrar === true);
 
     productosFiltrados.forEach(producto => {
-        if (producto.categoria) {
+        if (producto.categoria) and producto.mostrar === true {
             const item = document.createElement('div');
             item.classList.add('item');
             item.setAttribute('data-filter', producto.categoria);
