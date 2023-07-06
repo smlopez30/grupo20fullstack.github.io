428fd4ba53e1bdd5fa81319e6e8e9c395ef76892
smlopez30 <smlopez@gmail.com>
2023-07-06 17:50:03 -0300

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -63,17 +63,14 @@ createApp({
             }
         },
 
-        actualizarProducto(producto) {
+        actualizarProducto(producto, esImagen = false) {
             this.actualizando = true;
+
+            // Clonar el objeto producto original para comparar los cambios
+            const productoOriginal = JSON.parse(JSON.stringify(producto));
+
             const url = this.url + '/' + producto.id;
-            const data = {
-                nombre: producto.nombre,
-                precio: producto.precio,
-                stock: producto.stock,
-                imagen: producto.imagen,
-                categoria: producto.categoria,
-                mostrar: producto.mostrar 
-            };
+            const data = esImagen ? { imagen: producto.imagen } : producto;
             const options = {
                 body: JSON.stringify(data),
                 method: 'PUT',
@@ -83,6 +80,12 @@ createApp({
 
             fetch(url, options)
                 .then(() => {
+                    // Verificar si ha habido cambios en los campos del producto
+                    if (this.hayCambios(producto, productoOriginal)) {
+                        // Mostrar ventana emergente con los datos modificados en formato JSON
+                        this.mostrarVentanaEmergente(producto, productoOriginal, esImagen);
+                    }
+
                     this.fetchData(this.url);
                     this.actualizando = false;
                 })
@@ -91,6 +94,50 @@ createApp({
                     this.actualizando = false;
                 });
         },
+
+        hayCambios(producto, productoOriginal) {
+            // Verificar si ha habido cambios en los campos del producto
+            // Puedes agregar condiciones adicionales si hay más campos a tener en cuenta
+            return (
+                producto.nombre !== productoOriginal.nombre ||
+                producto.precio !== productoOriginal.precio ||
+                producto.stock !== productoOriginal.stock ||
+                producto.imagen !== productoOriginal.imagen ||
+                producto.categoria !== productoOriginal.categoria ||
+                producto.mostrar !== productoOriginal.mostrar
+            );
+        },
+
+        mostrarVentanaEmergente(producto, productoOriginal, esImagen) {
+            const cambios = [];
+            if (producto.nombre !== productoOriginal.nombre) {
+                cambios.push(`Nombre: ${JSON.stringify(productoOriginal.nombre)} -> ${JSON.stringify(producto.nombre)}`);
+            }
+            if (producto.precio !== productoOriginal.precio) {
+                cambios.push(`Precio: ${JSON.stringify(productoOriginal.precio)} -> ${JSON.stringify(producto.precio)}`);
+            }
+            if (producto.stock !== productoOriginal.stock) {
+                cambios.push(`Stock: ${JSON.stringify(productoOriginal.stock)} -> ${JSON.stringify(producto.stock)}`);
+            }
+            if (producto.imagen !== productoOriginal.imagen) {
+                cambios.push(`Imagen: ${JSON.stringify(productoOriginal.imagen)} -> ${JSON.stringify(producto.imagen)}`);
+            }
+            if (producto.categoria !== productoOriginal.categoria) {
+                cambios.push(`Categoría: ${JSON.stringify(productoOriginal.categoria)} -> ${JSON.stringify(producto.categoria)}`);
+            }
+            if (producto.mostrar !== productoOriginal.mostrar) {
+                cambios.push(`Mostrar: ${JSON.stringify(productoOriginal.mostrar)} -> ${JSON.stringify(producto.mostrar)}`);
+            }
+
+            // Mostrar ventana emergente con los cambios en formato JSON
+            Swal.fire({
+                title: 'Datos modificados',
+                html: `<pre>${cambios.join('\n')}</pre>`,
+                icon: 'info',
+                showConfirmButton: true,
+            });
+        },
+
 
         agregarNuevo() {
             if (this.nuevoProducto.nombre && this.nuevoProducto.precio && this.nuevoProducto.stock) {
