01b66b7931ef618dd5a41e7501fd451610696b0c
smlopez30 <smlopez@gmail.com>
2023-07-06 18:39:21 -0300

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -17,8 +17,6 @@ createApp({
                 categoria: '',
                 mostrar: false
             },
-            actualizando: false, // Agrega esta propiedad
-            showAlert: false,
         };
     },
     computed: {
@@ -48,31 +46,21 @@ createApp({
                 });
         },
         eliminar(id) {
-            const confirmacion = confirm("¿Estás seguro de que deseas eliminar este producto?");
-            if (confirmacion) {
-                const url = this.url + '/' + id;
-                var options = {
-                    method: 'DELETE',
-                };
-                fetch(url, options)
-                    .then(res => res.text())
-                    .then(res => {
-                        this.fetchData(this.url);
-                        alert("El producto se ha eliminado correctamente.");
-                    });
-            }
+            const url = this.url + '/' + id;
+            var options = {
+                method: 'DELETE',
+            };
+            fetch(url, options)
+                .then(res => res.text())
+                .then(res => {
+                    this.fetchData(this.url);
+                    alert("El producto se ha eliminado correctamente.");
+                });
         },
-
-        actualizarProducto(producto, esImagen = false) {
-            this.actualizando = true;
-
-            // Clonar el objeto producto original para comparar los cambios
-            const productoOriginal = JSON.parse(JSON.stringify(producto));
-
+        actualizarProducto(producto) {
             const url = this.url + '/' + producto.id;
-            const data = esImagen ? { imagen: producto.imagen } : producto;
             const options = {
-                body: JSON.stringify(data),
+                body: JSON.stringify(producto),
                 method: 'PUT',
                 headers: { 'Content-Type': 'application/json' },
                 redirect: 'follow',
@@ -80,65 +68,33 @@ createApp({
 
             fetch(url, options)
                 .then(() => {
-                    // Verificar si ha habido cambios en los campos del producto
-                    if (this.hayCambios(producto, productoOriginal)) {
-                        // Mostrar ventana emergente con los datos modificados en formato JSON
-                        this.mostrarVentanaEmergente(producto, productoOriginal, esImagen);
-                    }
-
+                    alert("El producto se ha actualizado correctamente.");
                     this.fetchData(this.url);
-                    this.actualizando = false;
                 })
                 .catch(err => {
                     console.error(err);
-                    this.actualizando = false;
+                    alert("Error al actualizar el producto.");
                 });
         },
+        actualizarImagen(producto) {
+            const url = this.url + '/' + producto.id;
+            const options = {
+                body: JSON.stringify(producto),
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                redirect: 'follow',
+            };
 
-        hayCambios(producto, productoOriginal) {
-            // Verificar si ha habido cambios en los campos del producto
-            // Puedes agregar condiciones adicionales si hay más campos a tener en cuenta
-            return (
-                producto.nombre !== productoOriginal.nombre ||
-                producto.precio !== productoOriginal.precio ||
-                producto.stock !== productoOriginal.stock ||
-                producto.imagen !== productoOriginal.imagen ||
-                producto.categoria !== productoOriginal.categoria ||
-                producto.mostrar !== productoOriginal.mostrar
-            );
+            fetch(url, options)
+                .then(() => {
+                    alert("La imagen se ha actualizado correctamente.");
+                    this.fetchData(this.url);
+                })
+                .catch(err => {
+                    console.error(err);
+                    alert("Error al actualizar la imagen.");
+                });
         },
-
-        mostrarVentanaEmergente(producto, productoOriginal, esImagen) {
-            const cambios = [];
-            if (producto.nombre !== productoOriginal.nombre) {
-                cambios.push(`Nombre: ${JSON.stringify(productoOriginal.nombre)} -> ${JSON.stringify(producto.nombre)}`);
-            }
-            if (producto.precio !== productoOriginal.precio) {
-                cambios.push(`Precio: ${JSON.stringify(productoOriginal.precio)} -> ${JSON.stringify(producto.precio)}`);
-            }
-            if (producto.stock !== productoOriginal.stock) {
-                cambios.push(`Stock: ${JSON.stringify(productoOriginal.stock)} -> ${JSON.stringify(producto.stock)}`);
-            }
-            if (producto.imagen !== productoOriginal.imagen) {
-                cambios.push(`Imagen: ${JSON.stringify(productoOriginal.imagen)} -> ${JSON.stringify(producto.imagen)}`);
-            }
-            if (producto.categoria !== productoOriginal.categoria) {
-                cambios.push(`Categoría: ${JSON.stringify(productoOriginal.categoria)} -> ${JSON.stringify(producto.categoria)}`);
-            }
-            if (producto.mostrar !== productoOriginal.mostrar) {
-                cambios.push(`Mostrar: ${JSON.stringify(productoOriginal.mostrar)} -> ${JSON.stringify(producto.mostrar)}`);
-            }
-
-            // Mostrar ventana emergente con los cambios en formato JSON
-            Swal.fire({
-                title: 'Datos modificados',
-                html: `<pre>${cambios.join('\n')}</pre>`,
-                icon: 'info',
-                showConfirmButton: true,
-            });
-        },
-
-
         agregarNuevo() {
             if (this.nuevoProducto.nombre && this.nuevoProducto.precio && this.nuevoProducto.stock) {
                 const options = {
