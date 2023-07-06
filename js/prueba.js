6552cafa3ed8579f7743a267bbdc8e8a52623931
smlopez30 <smlopez@gmail.com>
2023-07-06 09:39:02 -0300

    Update prueba.js

--- js/prueba.js
+++ js/prueba.js
@@ -1,5 +1,3 @@
-const { createApp } = Vue;
-
 createApp({
     data() {
         return {
@@ -17,7 +15,6 @@ createApp({
                 categoria: '',
                 mostrar: false
             },
-            imagenesDisponibles: []
         };
     },
     computed: {
@@ -46,7 +43,6 @@ createApp({
                     this.error = true;
                 });
         },
-        
         eliminar(id) {
             const url = this.url + '/' + id;
             var options = {
@@ -60,43 +56,6 @@ createApp({
                 });
         },
         actualizarProducto(producto) {
-            if (producto.imagen.startsWith('http')) {
-                // Si la imagen es una URL, guardarla en la carpeta ../img
-                const extension = producto.imagen.substring(producto.imagen.lastIndexOf('.'));
-                const nombreArchivo = `${producto.id}${extension}`;
-                const urlImagen = `../img/${nombreArchivo}`;
-
-                fetch(producto.imagen)
-                    .then(response => response.blob())
-                    .then(blob => {
-                        const file = new File([blob], nombreArchivo, { type: blob.type });
-                        const formData = new FormData();
-                        formData.append('file', file);
-
-                        fetch(urlImagen, {
-                            method: 'POST',
-                            body: formData
-                        })
-                            .then(() => {
-                                // Actualizar la imagen en el producto y la base de datos
-                                producto.imagen = urlImagen;
-                                this.actualizarProductoEnBaseDeDatos(producto);
-                            })
-                            .catch(err => {
-                                console.error(err);
-                                alert("Error al cargar la imagen desde la URL.");
-                            });
-                    })
-                    .catch(err => {
-                        console.error(err);
-                        alert("Error al obtener la imagen desde la URL.");
-                    });
-            } else {
-                // La imagen es una ruta de archivo local o una URL personalizada
-                this.actualizarProductoEnBaseDeDatos(producto);
-            }
-        },
-        actualizarProductoEnBaseDeDatos(producto) {
             const url = this.url + '/' + producto.id;
             const data = {
                 nombre: producto.nombre,
@@ -129,21 +88,14 @@ createApp({
         },
         agregarNuevo() {
             if (this.nuevoProducto.nombre && this.nuevoProducto.precio && this.nuevoProducto.stock) {
-                const data = {
-                    nombre: this.nuevoProducto.nombre,
-                    precio: this.nuevoProducto.precio,
-                    stock: this.nuevoProducto.stock,
-                    imagen: this.nuevoProducto.imagen,
-                    categoria: this.nuevoProducto.categoria,
-                    mostrar: this.nuevoProducto.mostrar
-                };
-
-                fetch(this.url, {
+                const options = {
+                    body: JSON.stringify(this.nuevoProducto),
                     method: 'POST',
-                    body: JSON.stringify(data),
                     headers: { 'Content-Type': 'application/json' },
                     redirect: 'follow',
-                })
+                };
+
+                fetch(this.url, options)
                     .then(() => {
                         this.fetchData(this.url);
                         alert("El artículo se ha guardado correctamente.");
@@ -164,13 +116,11 @@ createApp({
                 alert("Por favor, complete todos los campos obligatorios.");
             }
         },
-
         changePage(page) {
             this.currentPage = page;
         },
     },
     created() {
         this.fetchData(this.url);
-        this.fetchImagenesDisponibles();
     },
 }).mount('#app');
