0dbec9f65ff122a768b9b0724d33fe8cf09d8442
smlopez30 <smlopez@gmail.com>
2023-07-09 01:38:14 -0300

    Update mostrar.js

--- js/mostrar.js
+++ js/mostrar.js
@@ -16,14 +16,11 @@ window.addEventListener('DOMContentLoaded', async () => {
 
 // Función para mostrar los productos en el HTML
 function mostrarProductos(productos) {
-    // Filtrar los productos en los que mostrar es verdadero
-    const productosMostrados = productos.filter(producto => producto.mostrar);
-
     new Vue({
         el: '#app',
         data() {
             return {
-                productos: productosMostrados,
+                productos: productos,
                 sortOption: 'nombre' // Opción de ordenamiento inicial
             };
         },
