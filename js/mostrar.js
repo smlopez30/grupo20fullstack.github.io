7c59197d728721d26cdd7d6f937acef4017c3680
smlopez30 <smlopez@gmail.com>
2023-07-09 01:32:53 -0300

    Update mostrar.js

--- js/mostrar.js
+++ js/mostrar.js
@@ -1,26 +1,12 @@
-// Cargar los productos al cargar la página
-window.addEventListener('DOMContentLoaded', async () => {
-    try {
-        const url = 'https://slopez.pythonanywhere.com/productos';
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            mostrarProductos(data);
-        } else {
-            throw new Error('Error al cargar los productos.');
-        }
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-// Función para mostrar los productos en el HTML
 function mostrarProductos(productos) {
+    // Filtrar los productos en los que mostrar es verdadero
+    const productosMostrados = productos.filter(producto => producto.mostrar);
+
     new Vue({
         el: '#app',
         data() {
             return {
-                productos: productos,
+                productos: productosMostrados,
                 sortOption: 'nombre' // Opción de ordenamiento inicial
             };
         },
