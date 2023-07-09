984c48d9c73205220e1b01ad6626844fe0ab3f46
smlopez30 <smlopez@gmail.com>
2023-07-09 01:52:16 -0300

    Update bue.js

--- js/bue.js
+++ js/bue.js
@@ -1,132 +1,46 @@
-const { createApp } = Vue;
+// Cargar los productos al cargar la página
+window.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const url = 'https://slopez.pythonanywhere.com/productos';
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            mostrarProductos(data);
+        } else {
+            throw new Error('Error al cargar los productos.');
+        }
+    } catch (error) {
+        console.error(error);
+    }
+});
 
-createApp({
-    data() {
-        return {
-            productos: [],
-            url: 'https://slopez.pythonanywhere.com/productos',
-            error: false,
-            cargando: true,
-            pageSize: 5,
-            currentPage: 1,
-            nuevoProducto: {
-                nombre: '',
-                precio: '',
-                stock: '',
-                imagen: '',
-                categoria: '',
-                mostrar: false
-            },
-            actualizando: false, // Agrega esta propiedad
-            showAlert: false,
-        };
-    },
-    computed: {
-        totalItems() {
-            return this.productos.length;
+// Función para mostrar los productos en el HTML
+function mostrarProductos(productos) {
+    new Vue({
+        el: '#app',
+        data() {
+            return {
+                productos: productos,
+                sortOption: 'nombre' // Opción de ordenamiento inicial
+            };
         },
-        totalPages() {
-            return Math.ceil(this.totalItems / this.pageSize);
-        },
-        paginatedProductos() {
-            const startIndex = (this.currentPage - 1) * this.pageSize;
-            const endIndex = startIndex + this.pageSize;
-            return this.productos.slice(startIndex, endIndex);
-        },
-    },
-    methods: {
-        fetchData(url) {
-            fetch(url)
-                .then(response => response.json())
-                .then(data => {
-                    this.productos = data;
-                    this.cargando = false;
-                })
-                .catch(err => {
-                    console.error(err);
-                    this.error = true;
-                });
-        },
-        eliminar(id) {
-            const confirmacion = confirm("¿Estás seguro de que deseas eliminar este producto?");
-            if (confirmacion) {
-                const url = this.url + '/' + id;
-                var options = {
-                    method: 'DELETE',
-                };
-                fetch(url, options)
-                    .then(res => res.text())
-                    .then(res => {
-                        this.fetchData(this.url);
-                        alert("El producto se ha eliminado correctamente.");
-                    });
+        computed: {
+            sortedProductos() {
+                if (this.sortOption === 'nombre') {
+                    return this.productos.sort((a, b) => a.nombre.localeCompare(b.nombre));
+                } else if (this.sortOption === 'precio') {
+                    return this.productos.sort((a, b) => a.precio - b.precio);
+                }
+                return this.productos;
             }
         },
-
-        actualizarProducto(producto) {
-            this.actualizando = true;
-            const url = this.url + '/' + producto.id;
-            const data = {
-                nombre: producto.nombre,
-                precio: producto.precio,
-                stock: producto.stock,
-                imagen: producto.imagen,
-                categoria: producto.categoria,
-                mostrar: producto.mostrar
-            };
-            const options = {
-                body: JSON.stringify(data),
-                method: 'PUT',
-                headers: { 'Content-Type': 'application/json' },
-                redirect: 'follow',
-            };
-
-            fetch(url, options)
-                .then(() => {
-                    this.fetchData(this.url);
-                    this.actualizando = false;
-                })
-                .catch(err => {
-                    console.error(err);
-                    this.actualizando = false;
-                });
-        },
-
-        agregarNuevo() {
-            if (this.nuevoProducto.nombre && this.nuevoProducto.precio && this.nuevoProducto.stock) {
-                const options = {
-                    body: JSON.stringify(this.nuevoProducto),
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    redirect: 'follow',
-                };
-
-                fetch(this.url, options)
-                    .then(() => {
-                        alert("El artículo se ha guardado correctamente.");
-                        this.fetchData(this.url);
-                        this.nuevoProducto = {
-                            nombre: '',
-                            precio: '',
-                            stock: '',
-                            imagen: '',
-                            categoria: '',
-                            mostrar: false
-                        };
-                    })
-                    .catch(err => {
-                        console.error(err);
-                        alert("Error al guardar el artículo.");
-                    });
-            } else {
-                alert("Por favor, complete todos los campos obligatorios.");
+        methods: {
+            setFilter(categoria) {
+                this.filter = categoria;
             }
         },
-        changePage(page) {
-            this.currentPage = page;
-        },
-    },
-    created() {
-        this.fetchData(this.url);
-    },
-}).mount('#app');
+        mounted() {
+            Vue.config.productionTip = false;
+        }
+    });
+}
